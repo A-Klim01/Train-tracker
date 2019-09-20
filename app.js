080006eeb5d00608a3657031894c8818d51ed47b
Alojzy Klim <adklim@gmail.com>
2019-09-19 21:54:16 -0500

    added code in HTML and added firebase

---
+++ app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCbFUbD9JUZASC9HottPis9p3_88IGzV5M",
+    authDomain: "train-assignment-91c72.firebaseapp.com",
+    databaseURL: "https://train-assignment-91c72.firebaseio.com",
+    projectId: "train-assignment-91c72",
+    storageBucket: "",
+    messagingSenderId: "736028027232",
+    appId: "1:736028027232:web:4b20160e365cbae3fc1605"}
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
